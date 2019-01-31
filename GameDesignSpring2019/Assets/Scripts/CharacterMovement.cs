9febdd792b44593575ec015b28e552d5b2537011
Unknown <kosukeinuzuka@gmail.com>
2019-01-30 17:48:52 -0700

    Got horizontal movement working, jumping not so much

---
+++ GameDesignSpring2019/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+
+public class CharacterMovement : MonoBehaviour 
+{
+	
+	private CharacterController controller;
+	public MovePattern pattern;
+
+	private void Start()
+	{
+		controller = GetComponent<CharacterController>();
+	}
+
+	void Update()
+	{
+		pattern.Invoke(controller, transform);
+	}
+}
+
