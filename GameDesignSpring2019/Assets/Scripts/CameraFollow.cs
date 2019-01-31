c361684dd4341f7a77e3316b5120529a01ae35f1
Unknown <kosukeinuzuka@gmail.com>
2019-01-30 20:17:15 -0700

    set up camera follow

---
+++ GameDesignSpring2019/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+	public GameObject player;
+	private Vector3 Offset;
+
+	private void Start()
+	{
+		Offset = transform.position - player.transform.position;
+	}
+
+	private void Update()
+	{
+		transform.position = player.transform.position + Offset;
+	}
+}
