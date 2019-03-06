9ff48def43300235e56e013f18136e406d586c52
Unknown <kosukeinuzuka@gmail.com>
2019-03-05 21:26:16 -0700

    started adding Anthony's scripts

---
+++ GameDesign2019/Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MoveCharacter : MonoBehaviour
+{
+    public UnityEvent OnGrounded, OffGrounded;
+    public MoveBase CharacterMover;
+    private CharacterController controller;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (controller.isGrounded)
+        {
+            OnGrounded.Invoke();
+        }
+        else
+        {
+            OffGrounded.Invoke();
+        }
+
+        CharacterMover.Move(controller);
+    }
+}
