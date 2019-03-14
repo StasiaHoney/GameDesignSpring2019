1a50a35aadbfb1149b894475dedd2e7aa26eb132
Unknown <kosukeinuzuka@gmail.com>
2019-03-13 23:03:20 -0600

    attempted to work on nav mesh, didn't work very well

---
+++ GameDesign2019/Assets/Scripts/MoveFlat/MoveWithSingleJumpFlat.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class MoveWithSingleJumpFlat : MoveBase
+{
+   public override void Move(CharacterController controller)
+   {   
+      position.z += Gravity * Time.deltaTime;
+      if (controller.isGrounded && Input.GetButton("Jump"))
+      {
+         position.z = JumpForce;
+      }
+      else if (controller.isGrounded)
+      {
+         position.z = 0;
+      }
+
+      
+      position.x = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+      controller.Move(position);
+   }
+}
