a9415820396c7abfb570d24c53210e7ed3d3ba7d
Unknown <kosukeinuzuka@gmail.com>
2019-04-26 16:57:45 -0600

    started on environment anims and enemy slow down

---
+++ GameDesign2019/Assets/Scripts/SlowEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SlowEnemy : TrapInteract
+{
+    public NavMeshAgent agent;
+    public float enemySpeed = 22f;
+    public float enemyKnockBack = 3f;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (TrapActivated)
+        {
+            agent.speed = enemyKnockBack;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        agent.speed = enemySpeed;
+    }
+}
---
+++ GameDesign2019/Assets/Scripts/TrapInteract.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapInteract : MonoBehaviour
+{
+    public bool TrapActivated;
+    
+    private void OnMouseDown()
+    {
+        TrapActivated = true;
+    }
+}
