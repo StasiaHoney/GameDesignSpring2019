c326368e9c2c3eead55625dbdf955b3ef7280f66
Unknown <kosukeinuzuka@gmail.com>
2019-04-30 11:19:40 -0600

    added nightsky and finished swinging trap

--- GameDesign2019/Assets/Scripts/Weapon/RotateAroundPivot.cs
+++ GameDesign2019/Assets/Scripts/Weapon/RotateAroundPivot.cs
@@ -1,19 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
 
 public class RotateAroundPivot : TrapInteract
 {
     public GameObject trap;
     private Vector3 downForce;
-
-    public float value = -1;
+    public float negValue = -100;
     
     void Update()
     {
         if (TrapActivated)
-        {
-            downForce.z = value;
+        {   downForce.z = negValue *Time.deltaTime;
             trap.transform.Rotate(downForce);
         }
     }
